dfc5ad90b3d1cdd79e9cb48848e444dddabf698f
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-06-02 10:35:15 +0100

    Change streak label to be more explicit

--- apps/habits-frontend/components/streak-badge.tsx
+++ apps/habits-frontend/components/streak-badge.tsx
@@ -9,7 +9,7 @@ const StreakBadge = ({ days }: IStreakBadge) => {
 
   return (
     <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 ml-1.5">
-      {days} days
+      {days} day streak
     </span>
   );
 };
